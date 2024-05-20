53596f3a0f2648b527bf879b3302ac436971bb56
akshith <nakshith_csb223244@mgit.ac.in>
2024-05-20 12:53:20 +0530

    chore: Update npm dependencies and add lazy loading for images

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
 export default authMiddleware({
-  publicRoutes:['/']
-}
-);
-
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
+ 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
